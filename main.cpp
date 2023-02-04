4e55ef915c4d9de9158614624fd896c5a1f20eb3
jkeefe@rada-inno.com <jkeefe@rada-inno.com>
2023-02-04 17:12:13 -0500

    Added version number, argument, function call with argument.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
+//#include <cstdlib>
 #include <iostream>
 #include <vector>  
-   
+
+#include "version.h"
+
 using namespace std;
 
 std::vector<std::string> list;
 
-int func1()
+int func1(int count)
 {
-    list.push_back("test1");    
-    list.push_back("test2");
-    list.push_back("test3");
-
+    for (int index = 1; index < count + 1; index++)
+    {
+        std:string value = "test" + std::to_string(index);
+        list.push_back(value);    
+    }  
+    
     return list.size();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "Hello World!" << endl;
+   
+    std::cout << argv[0] << " Version " 
+        << cpp_VERSION_MAJOR << "."
+        << cpp_VERSION_MINOR << "." 
+        << cpp_VERSION_PATCH << std::endl;
 
-    int result = func1();
+    if (argc != 2)    
+    {
+        std::cout << "Usage: " << argv[0] << " <number>" << std::endl;    
+        return -1;
+    }
+
+    const int inputValue = std::stoi(argv[1]);
+        
+    int result = func1(inputValue);
 
     for (int index = 0; index < result; index++)
     {
---
+++ version.h
@@ -0,0 +1,3 @@
+#define cpp_VERSION_MAJOR 1
+#define cpp_VERSION_MINOR 0
+#define cpp_VERSION_PATCH 1
